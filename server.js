dbfcd2f379567221d17c6e8d221a18aabf574f41
smj <sunmingjie@winupon.com>
2023-04-07 17:29:07 +0800

    fix: 调整server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require("express"); // 先安装 npm i express -D
 const path = require("path");
 const app = express();
 app.use(express.static(path.resolve(__dirname, "dist"), { maxAge: 3600000 }))
-app.listen(3000);
-console.log("app listen 3000");
+app.listen(3001);
+console.log("app listen 3001");
